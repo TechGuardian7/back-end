973a1efe09015f6a2e45ccc4c0bf540a25e40bd9
Vitor-y <yurivitor022@gmail.com>
2024-04-17 23:08:37 -0300

    feat: atualizando dados
    método para atualizar os dados de registro
    
    Vitor - Andamento da Task TGN-23

--- spring/src/main/java/techguardian/api/controller/EntradaController.java
+++ spring/src/main/java/techguardian/api/controller/EntradaController.java
@@ -3,9 +3,12 @@ package techguardian.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +31,12 @@ public class EntradaController {
     @PostMapping("/entrada")
     public void receberDadosEntrada(@RequestBody DadosEntrada dadosEntrada) {
         entradaService.processarDadosEntrada(dadosEntrada);
+    }
+
+    @PutMapping("/entrada/{id}")
+    public ResponseEntity<String> atualizarEntrada(@PathVariable Long id, @RequestBody DadosEntrada dadosEntrada) {
+        entradaService.atualizarEntrada(id, dadosEntrada);
+        return ResponseEntity.ok("Entrada atualizada com sucesso");
     }
 
 }
--- spring/src/main/java/techguardian/api/service/EntradaService.java
+++ spring/src/main/java/techguardian/api/service/EntradaService.java
@@ -1,6 +1,8 @@
 package techguardian.api.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,5 +29,19 @@ public class EntradaService {
         registroEntrada.setObsEntrada(dadosEntrada.getObsEntrada());
 
         entradaRepo.save(registroEntrada);
+    }
+
+    public void atualizarEntrada(Long id, DadosEntrada dadosEntrada) {
+        Optional<RegistroEntrada> optionalEntrada = entradaRepo.findById(id);
+        if (optionalEntrada.isPresent()) {
+            RegistroEntrada entradaExistente = optionalEntrada.get();
+            entradaExistente.setDataEntrada(dadosEntrada.getDataEntrada());
+            entradaExistente.setHoraEntrada(dadosEntrada.getHoraEntrada());
+            entradaExistente.setQuantEntrada(dadosEntrada.getQuantEntrada());
+            entradaExistente.setObsEntrada(dadosEntrada.getObsEntrada());
+            entradaRepo.save(entradaExistente);
+        } else {
+            throw new NoSuchElementException("Entrada com o ID " + id + " não encontrada");
+        }
     }
 }
