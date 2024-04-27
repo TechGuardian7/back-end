c81eb5ed5c4bf1f5c5563ae058d5758b6fad9dd3
Vitor-y <yurivitor022@gmail.com>
2024-04-27 00:34:13 -0300

    feat: connecting with database
    excel pulling data from database
    
    Vitor - Progress TGN-29

--- spring/src/main/java/techguardian/api/controller/RecordController.java
+++ spring/src/main/java/techguardian/api/controller/RecordController.java
@@ -2,11 +2,13 @@ package techguardian.api.controller;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,14 +16,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import techguardian.api.entity.Input;
+import techguardian.api.entity.Output;
+import techguardian.api.service.InputService;
+import techguardian.api.service.OutputService;
+
 @RestController
 @RequestMapping("/registro/excel")
 public class RecordController {
+
+    @Autowired
+    private InputService inputService;
+
+    @Autowired
+    private OutputService outputService;
 
     @GetMapping("/entrada")
     public ResponseEntity<byte[]> recordInput() {
         try (Workbook workbook = new XSSFWorkbook()) {
             Sheet sheet = workbook.createSheet("Registro_Entrada");
+
+            List<Input> inputs = inputService.findAll();
 
             Row headerRow = sheet.createRow(0);
             headerRow.createCell(0).setCellValue("Data de Entrada");
@@ -29,18 +44,14 @@ public class RecordController {
             headerRow.createCell(2).setCellValue("Quantidade");
             headerRow.createCell(3).setCellValue("Observação");
 
-            Row dataRow1 = sheet.createRow(1);
-            dataRow1.createCell(0).setCellValue("19/04/2024");
-            dataRow1.createCell(1).setCellValue("14:15:23");
-            dataRow1.createCell(2).setCellValue(1);
-            dataRow1.createCell(3).setCellValue("Registrado a Entrada");
-
-
-            Row dataRow2 = sheet.createRow(2);
-            dataRow2.createCell(0).setCellValue("20/04/2024");
-            dataRow2.createCell(1).setCellValue("15:23:47");
-            dataRow2.createCell(2).setCellValue(1);
-            dataRow2.createCell(3).setCellValue("Registrado a Entrada");
+            int rowNum = 1;
+            for (Input input : inputs) {
+                Row row = sheet.createRow(rowNum++);
+                row.createCell(0).setCellValue(input.getDataEntrada());
+                row.createCell(1).setCellValue(input.getHoraEntrada());
+                row.createCell(2).setCellValue(input.getQuantEntrada());
+                row.createCell(3).setCellValue(input.getObsEntrada());
+            }
 
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             workbook.write(outputStream);
@@ -63,24 +74,22 @@ public class RecordController {
         try (Workbook workbook = new XSSFWorkbook()) {
             Sheet sheet = workbook.createSheet("Registro_Saida");
 
+            List<Output> outputs = outputService.findAll();
+
             Row headerRow = sheet.createRow(0);
             headerRow.createCell(0).setCellValue("Data da Saida");
             headerRow.createCell(1).setCellValue("Hora da Saida");
             headerRow.createCell(2).setCellValue("Quantidade");
             headerRow.createCell(3).setCellValue("Observação");
 
-            Row dataRow1 = sheet.createRow(1);
-            dataRow1.createCell(0).setCellValue("19/04/2024");
-            dataRow1.createCell(1).setCellValue("14:20:12");
-            dataRow1.createCell(2).setCellValue(1);
-            dataRow1.createCell(3).setCellValue("Registrado a Saida");
-
-
-            Row dataRow2 = sheet.createRow(2);
-            dataRow2.createCell(0).setCellValue("20/04/2024");
-            dataRow2.createCell(1).setCellValue("15:30:00");
-            dataRow2.createCell(2).setCellValue(1);
-            dataRow2.createCell(3).setCellValue("Registrado a Saida");
+            int rowNum = 1;
+            for (Output output : outputs) {
+                Row row = sheet.createRow(rowNum++);
+                row.createCell(0).setCellValue(output.getDataSaida());
+                row.createCell(1).setCellValue(output.getHoraSaida());
+                row.createCell(2).setCellValue(output.getQuantSaida());
+                row.createCell(3).setCellValue(output.getObsSaida());
+            }
 
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             workbook.write(outputStream);
