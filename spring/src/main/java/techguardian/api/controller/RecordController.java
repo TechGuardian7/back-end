1315f33d212fbfaf06ce0a903bbcbc1fdd3aa118
Vitor-y <yurivitor022@gmail.com>
2024-05-01 22:04:23 -0300

    feat: added chart
    adding a chart in excel for better visualization
    
    Vitor - Finalized TGN-29

--- spring/src/main/java/techguardian/api/controller/RecordController.java
+++ spring/src/main/java/techguardian/api/controller/RecordController.java
@@ -8,13 +8,27 @@ import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.FillPatternType;
-import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.IndexedColors;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xddf.usermodel.chart.AxisCrosses;
+import org.apache.poi.xddf.usermodel.chart.AxisPosition;
+import org.apache.poi.xddf.usermodel.chart.AxisTickMark;
+import org.apache.poi.xddf.usermodel.chart.ChartTypes;
+import org.apache.poi.xddf.usermodel.chart.LegendPosition;
+import org.apache.poi.xddf.usermodel.chart.XDDFCategoryAxis;
+import org.apache.poi.xddf.usermodel.chart.XDDFChartData;
+import org.apache.poi.xddf.usermodel.chart.XDDFChartLegend;
+import org.apache.poi.xddf.usermodel.chart.XDDFDataSource;
+import org.apache.poi.xddf.usermodel.chart.XDDFDataSourcesFactory;
+import org.apache.poi.xddf.usermodel.chart.XDDFNumericalDataSource;
+import org.apache.poi.xddf.usermodel.chart.XDDFValueAxis;
+import org.apache.poi.xssf.usermodel.XSSFChart;
+import org.apache.poi.xssf.usermodel.XSSFClientAnchor;
+import org.apache.poi.xssf.usermodel.XSSFDrawing;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -41,13 +55,14 @@ public class RecordController {
 
     @GetMapping("/entrada")
     public ResponseEntity<byte[]> generateExcel() {
-        try (Workbook workbook = new XSSFWorkbook()) {
-            Sheet sheet = workbook.createSheet("Registro_Entrada");
+        try (XSSFWorkbook workbook = new XSSFWorkbook()) {
+            XSSFSheet sheet = workbook.createSheet("Registro_Entrada");
 
             List<Input> inputs = inputService.findAll();
 
+            // Criando estilo de cabeçalho
             CellStyle headerStyle = workbook.createCellStyle();
-            Font headerFont = workbook.createFont();
+            XSSFFont headerFont = workbook.createFont();
             headerFont.setBold(true);
             headerStyle.setFont(headerFont);
             headerStyle.setFillForegroundColor(IndexedColors.PALE_BLUE.getIndex());
@@ -58,43 +73,71 @@ public class RecordController {
             headerStyle.setBorderRight(BorderStyle.THIN);
             headerStyle.setAlignment(HorizontalAlignment.CENTER);
 
-            Row headerRow = sheet.createRow(0);
+            // Criando cabeçalho
+            XSSFRow headerRow = sheet.createRow(0);
             headerRow.createCell(0).setCellValue("ID");
             headerRow.createCell(1).setCellValue("Data de Entrada");
             headerRow.createCell(2).setCellValue("Hora de Entrada");
             headerRow.createCell(3).setCellValue("Quantidade de Entrada");
             headerRow.createCell(4).setCellValue("Observações");
 
+            // Aplicando estilo ao cabeçalho
             for (Cell cell : headerRow) {
                 cell.setCellStyle(headerStyle);
             }
 
+            // Preenchendo dados do banco de dados
             int rowNum = 1;
             for (Input input : inputs) {
-                Row row = sheet.createRow(rowNum++);
+                XSSFRow row = sheet.createRow(rowNum++);
                 row.createCell(0).setCellValue(input.getId());
                 row.createCell(1).setCellValue(input.getDataEntrada());
                 row.createCell(2).setCellValue(input.getHoraEntrada());
                 row.createCell(3).setCellValue(input.getQuantEntrada());
                 row.createCell(4).setCellValue(input.getObsEntrada());
-
-                for (Cell cell : row) {
-                    cell.setCellStyle(headerStyle);
-                }
             }
 
+            // Criando tabela
             int rowCount = sheet.getLastRowNum();
             int columnCount = headerRow.getLastCellNum();
             CellRangeAddress tableRange = new CellRangeAddress(0, rowCount, 0, columnCount - 1);
             sheet.setAutoFilter(tableRange);
 
+            // Criando gráfico de colunas
+            XSSFDrawing drawing = sheet.createDrawingPatriarch();
+            XSSFClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, 5, 1, 15, 15);
+            XSSFChart chart = drawing.createChart(anchor);
+            XDDFChartLegend legend = chart.getOrAddLegend();
+            legend.setPosition(LegendPosition.TOP_RIGHT);
+
+            XDDFCategoryAxis bottomAxis = chart.createCategoryAxis(AxisPosition.BOTTOM);
+            bottomAxis.setMajorTickMark(AxisTickMark.OUT);
+            bottomAxis.setMinorTickMark(AxisTickMark.NONE);
+            bottomAxis.setCrosses(AxisCrosses.AUTO_ZERO);
+
+            XDDFValueAxis leftAxis = chart.createValueAxis(AxisPosition.LEFT);
+            leftAxis.setMajorTickMark(AxisTickMark.OUT);
+            leftAxis.setMinorTickMark(AxisTickMark.NONE);
+            leftAxis.setCrosses(AxisCrosses.AUTO_ZERO);
+
+            XDDFDataSource<Double> xs = XDDFDataSourcesFactory.fromNumericCellRange(sheet, new CellRangeAddress(1, rowCount, 3, 3));
+            XDDFNumericalDataSource<Double> ys = XDDFDataSourcesFactory.fromNumericCellRange(sheet, new CellRangeAddress(1, rowCount, 0, 0));
+
+            XDDFChartData data = chart.createData(ChartTypes.BAR, bottomAxis, leftAxis);
+            XDDFChartData.Series series = data.addSeries(xs, ys);
+            series.setTitle("Quantidade de Entrada", null);
+            chart.plot(data);
+
+            // Ajustando largura das colunas automaticamente
             for (int i = 0; i < 5; i++) {
                 sheet.autoSizeColumn(i);
             }
 
+            // Escrevendo o arquivo Excel em um ByteArrayOutputStream
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             workbook.write(outputStream);
 
+            // Definindo os cabeçalhos da resposta HTTP
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
             headers.setContentDispositionFormData("filename", "Registro_Entrada.xlsx");
@@ -104,19 +147,20 @@ public class RecordController {
                     .body(outputStream.toByteArray());
         } catch (IOException e) {
             e.printStackTrace();
+            // Tratamento de erro
             return ResponseEntity.badRequest().build();
         }
     }
 
     @GetMapping("/saida")
     public ResponseEntity<byte[]> recordOutput() {
-        try (Workbook workbook = new XSSFWorkbook()) {
-            Sheet sheet = workbook.createSheet("Registro_Saida");
+        try (XSSFWorkbook workbook = new XSSFWorkbook()) {
+            XSSFSheet sheet = workbook.createSheet("Registro_Saida");
 
             List<Output> outputs = outputService.findAll();
 
             CellStyle headerStyle = workbook.createCellStyle();
-            Font headerFont = workbook.createFont();
+            XSSFFont headerFont = workbook.createFont();
             headerFont.setBold(true);
             headerStyle.setFont(headerFont);
             headerStyle.setFillForegroundColor(IndexedColors.PALE_BLUE.getIndex());
@@ -127,42 +171,70 @@ public class RecordController {
             headerStyle.setBorderRight(BorderStyle.THIN);
             headerStyle.setAlignment(HorizontalAlignment.CENTER);
 
-            Row headerRow = sheet.createRow(0);
-            headerRow.createCell(0).setCellValue("Data da Saida");
-            headerRow.createCell(1).setCellValue("Hora da Saida");
-            headerRow.createCell(2).setCellValue("Quantidade");
-            headerRow.createCell(3).setCellValue("Observação");
+            XSSFRow headerRow = sheet.createRow(0);
+            headerRow.createCell(0).setCellValue("ID");
+            headerRow.createCell(1).setCellValue("Data de Saida");
+            headerRow.createCell(2).setCellValue("Hora de Saida");
+            headerRow.createCell(3).setCellValue("Quantidade de Saida");
+            headerRow.createCell(4).setCellValue("Observações");
 
+            // Aplicando estilo ao cabeçalho
             for (Cell cell : headerRow) {
                 cell.setCellStyle(headerStyle);
             }
 
+            // Preenchendo dados do banco de dados
             int rowNum = 1;
             for (Output output : outputs) {
-                Row row = sheet.createRow(rowNum++);
-                row.createCell(0).setCellValue(output.getDataSaida());
-                row.createCell(1).setCellValue(output.getHoraSaida());
-                row.createCell(2).setCellValue(output.getQuantSaida());
-                row.createCell(3).setCellValue(output.getObsSaida());
-
-                for (Cell cell : row) {
-                    cell.setCellStyle(headerStyle);
-                }
+                XSSFRow row = sheet.createRow(rowNum++);
+                row.createCell(0).setCellValue(output.getId());
+                row.createCell(1).setCellValue(output.getDataSaida());
+                row.createCell(2).setCellValue(output.getHoraSaida());
+                row.createCell(3).setCellValue(output.getQuantSaida());
+                row.createCell(4).setCellValue(output.getObsSaida());
             }
 
+            // Criando tabela
             int rowCount = sheet.getLastRowNum();
             int columnCount = headerRow.getLastCellNum();
             CellRangeAddress tableRange = new CellRangeAddress(0, rowCount, 0, columnCount - 1);
             sheet.setAutoFilter(tableRange);
+
+            // Criando gráfico de colunas
+            XSSFDrawing drawing = sheet.createDrawingPatriarch();
+            XSSFClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, 5, 1, 15, 15);
+            XSSFChart chart = drawing.createChart(anchor);
+            XDDFChartLegend legend = chart.getOrAddLegend();
+            legend.setPosition(LegendPosition.TOP_RIGHT);
+
+            XDDFCategoryAxis bottomAxis = chart.createCategoryAxis(AxisPosition.BOTTOM);
+            bottomAxis.setMajorTickMark(AxisTickMark.OUT);
+            bottomAxis.setMinorTickMark(AxisTickMark.NONE);
+            bottomAxis.setCrosses(AxisCrosses.AUTO_ZERO);
+
+            XDDFValueAxis leftAxis = chart.createValueAxis(AxisPosition.LEFT);
+            leftAxis.setMajorTickMark(AxisTickMark.OUT);
+            leftAxis.setMinorTickMark(AxisTickMark.NONE);
+            leftAxis.setCrosses(AxisCrosses.AUTO_ZERO);
+
+            XDDFDataSource<Double> xs = XDDFDataSourcesFactory.fromNumericCellRange(sheet, new CellRangeAddress(1, rowCount, 3, 3));
+            XDDFNumericalDataSource<Double> ys = XDDFDataSourcesFactory.fromNumericCellRange(sheet, new CellRangeAddress(1, rowCount, 0, 0));
+
+            XDDFChartData data = chart.createData(ChartTypes.BAR, bottomAxis, leftAxis);
+            XDDFChartData.Series series = data.addSeries(xs, ys);
+            series.setTitle("Quantidade de Saida", null);
+            chart.plot(data);
 
             // Ajustando largura das colunas automaticamente
             for (int i = 0; i < 5; i++) {
                 sheet.autoSizeColumn(i);
             }
 
+            // Escrevendo o arquivo Excel em um ByteArrayOutputStream
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             workbook.write(outputStream);
 
+            // Definindo os cabeçalhos da resposta HTTP
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
             headers.setContentDispositionFormData("filename", "Registro_Saida.xlsx");
@@ -172,6 +244,7 @@ public class RecordController {
                     .body(outputStream.toByteArray());
         } catch (IOException e) {
             e.printStackTrace();
+            // Tratamento de erro
             return ResponseEntity.badRequest().build();
         }
     }
