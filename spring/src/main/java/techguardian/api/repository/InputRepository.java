b8be039b14ddc4a642f92bb2ec6f1d0b0972c92e
Vitor-y <yurivitor022@gmail.com>
2024-05-09 09:54:19 -0300

    fix: added missing annotations

--- spring/src/main/java/techguardian/api/repository/InputRepository.java
+++ spring/src/main/java/techguardian/api/repository/InputRepository.java
@@ -1,9 +1,11 @@
 package techguardian.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import techguardian.api.entity.Input;
 
+@Repository
 public interface InputRepository extends JpaRepository<Input, Long> {
 
 }
--- spring/src/main/java/techguardian/api/repository/OutputRepository.java
+++ spring/src/main/java/techguardian/api/repository/OutputRepository.java
@@ -1,9 +1,11 @@
 package techguardian.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import techguardian.api.entity.Output;
 
+@Repository
 public interface OutputRepository extends JpaRepository<Output, Long>{
 
 }
